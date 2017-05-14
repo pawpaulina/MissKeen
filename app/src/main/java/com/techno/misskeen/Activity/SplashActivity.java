c89d4f51973a22f7430f44c7df7c619d5ee46862
pawpaulina <paulina.sudharman.ps@gmail.com>
2017-05-14 21:56:20 +0700

    buat drawer, rename mainactivity->splash activity, tambah beberapa view fragment tp masih kosongan

---
+++ app/src/main/java/com/techno/misskeen/Activity/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.techno.misskeen.Activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.techno.misskeen.Fragment.RecipeListFragment;
+import com.techno.misskeen.Helper.PrefHelper;
+import com.techno.misskeen.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String mFlag = PrefHelper.getPref(getApplicationContext(),"ID");
+        if(mFlag == null)
+        {
+            new Handler().postDelayed(new Runnable() {
+                  @Override
+                  public void run() {
+                      Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                      startActivity(i);
+                  }
+            }, 5000);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    PrefHelper.saveToPref(getApplicationContext(),"ID","");
+                    Intent i = new Intent(getApplicationContext(), RecipeListFragment.class);
+                    startActivity(i);
+                }
+            }, 5000);
+        }
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
--- app/src/main/java/com/techno/misskeen/Client.java
+++ app/src/main/java/com/techno/misskeen/Client.java
@@ -3,6 +3,11 @@ package com.techno.misskeen;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.*;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,12 +15,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by Paulina on 5/9/2017.
  */
 public class Client {
-    public static final String BASE_URL = "http://ditoraharjo.co/misskeen/api/v1/test";
+    public static final String BASE_URL = "http://ditoraharjo.co/misskeen/api/v1/";
 
     private static Retrofit retrofit = null;
 
+
+
     public static Retrofit getClient()
     {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(interceptor) //  todo for debug
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build();
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
@@ -23,11 +37,32 @@ public class Client {
         if (retrofit==null)
         {
             retrofit = new Retrofit.Builder()
+                    .client(client)
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
     }
+    private class MyInterceptor implements Interceptor {
 
-}
+        @Override
+        public okhttp3.Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+
+            Request.Builder reqBuilder = original.newBuilder();
+//            if (mUserToken != null) {
+//                reqBuilder.addHeader("usertoken", mUserToken);
+//            }
+
+            Request request = reqBuilder.build();
+            okhttp3.Response response = chain.proceed(request);
+
+            String rawJson = response.body().string();
+
+                return response.newBuilder().body(ResponseBody.create(response.body().contentType(), rawJson)).build();
+            }
+        }
+
+
+    }
