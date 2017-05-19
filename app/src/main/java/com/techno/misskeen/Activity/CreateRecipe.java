0fb75ed102ede6bf8992bd2d250e5091c39e7953
Unknown <kusumayudha32@gmail.com>
2017-05-19 04:23:10 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/main/java/com/techno/misskeen/Activity/CreateRecipe.java
@@ -0,0 +1,15 @@
+package com.techno.misskeen.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.techno.misskeen.R;
+
+public class CreateRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe2);
+    }
+}
